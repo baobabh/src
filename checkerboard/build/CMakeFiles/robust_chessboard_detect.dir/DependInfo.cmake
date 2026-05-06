b0e3d9e7ed4a2783a934b2df81461c467d624d7c
baobabh <powerice007@gmail.com>
2026-05-06 23:19:59 +0900

    Add files via upload

---
+++ checkerboard/build/CMakeFiles/robust_chessboard_detect.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/gadeuk/Documents/졸업과제/checkerboard/improved/robust_chessboard_detect.cpp" "CMakeFiles/robust_chessboard_detect.dir/robust_chessboard_detect.cpp.o" "gcc" "CMakeFiles/robust_chessboard_detect.dir/robust_chessboard_detect.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ checkerboard/build/CMakeFiles/robust_chessboard_detect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robust_chessboard_detect.dir/robust_chessboard_detect.cpp.o"
+  "CMakeFiles/robust_chessboard_detect.dir/robust_chessboard_detect.cpp.o.d"
+  "robust_chessboard_detect"
+  "robust_chessboard_detect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robust_chessboard_detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
